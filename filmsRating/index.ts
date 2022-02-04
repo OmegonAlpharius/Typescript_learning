799f4647cccb3fb5a09ff6634ae0cea644987091
OmegonAlpharius <tng.tarasenko@gmail.com>
2022-02-04 22:17:28 +0600

    Добавиk функцию list

---
+++ filmsRating/index.ts
@@ -0,0 +1,48 @@
+interface IMovie {
+  name: string;
+  ratings: { [key: string]: number };
+}
+
+const movies: IMovie[] = [
+  {
+    name: "Interstellar",
+    ratings: {
+      John: 10,
+      Jack: 3,
+    },
+  },
+  {
+    name: "Avengers: Infinity War",
+    ratings: {
+      Jack: 9,
+      Jane: 10,
+      John: 10,
+    },
+  },
+];
+
+function list(movies: Array<IMovie>): void {
+  const maxLength: number = movies.reduce((max, movie) => {
+    if (max < movie.name.length) return movie.name.length;
+    return max;
+  }, 0);
+
+  const tableHeader: string =
+    `| № | movies name ${" ".repeat(maxLength - 12)} | rating  |\n` +
+    `--------------------${"-".repeat(maxLength - 12)}----------\n`;
+
+  const tableBody: string = movies.reduce((acc, movie, index) => {
+    const ratings: Array<number> = Object.values(movie.ratings);
+    const averageRating: number = ratings.reduce((sum: number, value: number): number => sum + value) / ratings.length;
+    return (
+      acc +
+      `| ${index + 1} | ${movie.name}${" ".repeat(maxLength - movie.name.length)} | ${averageRating.toFixed(
+        1
+      )}     |\n` +
+      `--------------------${"-".repeat(maxLength - 12)}----------\n`
+    );
+  }, "");
+  console.log(`${tableHeader}${tableBody}`);
+}
+
+list(movies);
