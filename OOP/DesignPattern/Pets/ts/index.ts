517c8774e37e9cc7e1e1d0347ec2bf3ba9cfdec6
TNG <tng.tarasenko@gmail.com>
2022-02-23 21:55:23 +0600

    add base class Game and Cat

--- OOP/DesignPattern/Pets/ts/index.ts
+++ OOP/DesignPattern/Pets/ts/index.ts
@@ -8,4 +8,28 @@ class Cat {
         this.age = age;
 
     }
+
+    letPlay():void {
+
+    }
+    letFeed():void {
+        
+    }
+    letTreat():void{
+
+    }
+}
+
+
+class Game {
+   private _pets:Cat[];
+
+ 
+    constructor(){
+        this._pets =[new Cat("Барсик",3)]
+    }
+    addPet(name:string,age:number):void{
+        this._pets.push(new Cat(name,age))
+    }
+  
 }
